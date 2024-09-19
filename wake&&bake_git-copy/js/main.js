3abb2a1a1cd7c2e16889fd6bb8267bbdaeaa4101
Eyes2Ice <jer_x_2014_projekt@mail.ru>
2024-09-19 14:01:56 +0300

    Add files via upload

--- wake&&bake_git-copy/js/main.js
+++ wake&&bake_git-copy/js/main.js
@@ -4,11 +4,12 @@
     function burgerInit(e) {
 
         const burgerIcon = e.target.closest('.burger-icon')
-        const burgerNavLink = e.target.closest('nav__link')
+        const burgerNavLink = e.target.closest('.nav__link')
 
         if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
 
-        if (!document.body.classList.contains('body--opened-menu')) {
+        if (!document.body.classList.contains('body--opened-menu')) {   
             document.body.classList.add('body--opened-menu')
         } else {
             document.body.classList.remove('body--opened-menu')
