6e30f55aff038738c41b4b110f4f59d9748649e8
Eyes2Ice <jer_x_2014_projekt@mail.ru>
2024-10-17 21:31:59 +0300

    Add files via upload

--- wake&&bake_git-copy/js/main.js
+++ wake&&bake_git-copy/js/main.js
@@ -1,3 +1,4 @@
+// ====================================Бургер===============================================
 (function () {
     document.addEventListener('click', burgerInit)
 
@@ -16,7 +17,7 @@
         }
     }
 
-    // ===================================================================================
+    // =================================Модалка================================================
 
     const modal = document.querySelector('.modal')
     const modalButton = document.querySelector('.about__img-button')
@@ -44,7 +45,31 @@
             document.body.classList.remove('body--opened-modal')
         }
     })
-    // ================================================================================
+    // =================================Табуляция==============================================
+
+    const tabControls = document.querySelector('.tab-controls')
+    tabControls.addEventListener('click', toggleTab)
+
+    function toggleTab(e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+        e.preventDefault()
+        if (tabControl.classList.contains('tab-controls__link--active')) return
+
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeContent = document.querySelector('.tab-content--show')
+        const activeControl = document.querySelector('.tab-controls__link--active')
+
+        activeControl.classList.remove('tab-controls__link--active')
+        activeContent.classList.remove('tab-content--show')
+
+        tabControl.classList.add('tab-controls__link--active')
+        tabContent.classList.add('tab-content--show')
+    }
+
 
 })()
 
