19c08aaa5ab7a642a29139aa2565fde1053d5f41
Eyes2Ice <jer_x_2014_projekt@mail.ru>
2024-11-09 13:54:47 +0300

    Add files via upload

--- manhattan/js/main.js
+++ manhattan/js/main.js
@@ -102,4 +102,39 @@
             },
         },
     });
+
+    // ===============================Аккордион=================================
+
+    const accordionList = document.querySelector('.accordion-list');
+
+    const firstAccordionItem = accordionList.querySelector('.accordion-list__item');
+    const firstAccordionContent = firstAccordionItem.querySelector('.accordion-list__content');
+
+    firstAccordionItem.classList.add('accordion-list__item--opened');
+    firstAccordionContent.style.maxHeight = firstAccordionContent.scrollHeight + 'px';
+
+    accordionList.addEventListener('click', (e) => {
+
+        const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened');
+        const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content');
+
+        const accordionControl = e.target.closest('.accordion-list__control');
+        if (!accordionControl) return
+        e.preventDefault();
+        const accordionItem = accordionControl.parentElement;
+        const accordionContent = accordionControl.nextElementSibling;
+
+        if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+            accordionOpenedItem.classList.remove('accordion-list__item--opened')
+            accordionOpenedContent.style.maxHeight = null;
+        }
+        accordionItem.classList.toggle('accordion-list__item--opened');
+
+        if (accordionItem.classList.contains('accordion-list__item--opened')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        } else {
+            accordionContent.style.maxHeight = null;
+        }
+    })
+
 })()
